a033bd8830ea7c8af908f988c2970bdbb7ec2b15
enachevivian <enache_vivian@yahoo.com>
2017-01-13 16:58:27 +0200

    Add files via upload

---
+++ RapidTyping/WebContent/js/words.js
@@ -0,0 +1,103 @@
+
+
+$( document ).ready(function() {
+	var array = [
+		//lvl1
+		'bells', 'candles', 'cards', 'celebrate', 'family reunion', 'Frosty',
+		'greetings', 'holiday', 'jubilee', 'merry', 'Noel', 'parades', 'party', 'red', 'Scrooge', 'season', 'spirit',
+		'stocking stuffers', 'stockings', 'tidings',
+		//lvl2
+		'tradition', 'traffic', 'trips', 'vacation', 'wassail',
+		'wassailing', 'yule', 'yuletide', 'chimney', 'elves', 'fairies', 'jolly', 'North Pole', 'reindeer', 'Rudolph',
+		'sled', 'sleigh', 'sleigh bells', 'St. Nick', 'toys',
+		//lvl3
+		'anticipating', 'excited', 'exhausted', 'happy', 'sad',
+		'pumpkin pie', 'punch', 'sauce', 'snowball', 'snowbound', 'nativity', 'vacation', 'exchange', 'icicle','carolers',
+		'angel','frankincense','celebration','blizzard','charity',
+		//lvl4
+		'You better watch out',
+		'You better not cry',
+		'Better not pout',
+		"I'm telling you why",
+		'Santa Claus is coming to town',
+
+		"He's making a list",
+		'And checking it twice',
+		"Gonna find out Who's naughty and nice",
+		'Santa Claus is coming to town',
+
+		"He sees you when you're sleeping",
+		"He knows when you're awake",
+		"He knows if you've been bad or good",
+		'So be good for goodness sake!',
+
+		'O! You better watch out!',
+		'You better not cry',
+		'Better not pout',
+		"I'm telling you why",
+		'Santa Claus is coming to town',
+
+		"It's the most wonderful time of the year",
+		"With the kids jingle belling",
+		//lvl5
+		"And everyone telling you 'Be of good cheer'",
+		"It's the most wonderful time of the year",
+		"It's the hap-happiest season of all",
+		"When friends come to call",
+		"It's the hap-happiest season of all",
+
+		"There'll be parties for hosting",
+		"Marshmallows for toasting",
+		"And caroling out in the snow", 
+		"There'll be scary ghost stories", 
+		"And tales of the glories of",
+		"Christmases long, long ago",
+
+		"It's the most wonderful time of the year",
+		"There'll be much mistletoeing",
+		"And hearts will be glowing",
+		"When love ones are near",
+		"It's the most wonderful time of the year",
+
+		"Santa baby, just slip a sable under the tree for me;",
+		"Been an awful good girl Santa baby",
+		"and hurry down the chimney tonight",
+
+		"Santa baby a '54 convertible too light blue",
+		//lvl6
+		"I'll wait up for you dear Santa baby",
+		"and hurry down the chimney tonight",
+
+		"Think of all the fun I've missed",
+		"Think of all the fellas that I haven't kissed", 
+		"Next year I could be just as good...",
+		"if you'd check off my Christmas list",
+
+		"Santa baby, I want a yacht and really that's not a lot",
+		"Been an angel all year Santa baby,",
+		"so hurry down the chimney tonight",
+
+		"Santa honey there's one thing I really do need,",
+		"the deed - To a platinum mine, Santa baby",
+		"so hurry down the chimney tonight",
+
+		"Have yourself a merry little Christmas",
+		"Let your heart be light",
+		"From now on",
+		"our troubles will be out of sight", 
+		"Have yourself a merry little Christmas",
+		"From now on",
+		"our troubles will be miles away",
+
+		"Here were are as in olden days",
+		"happy golden days of yore",
+		"Faithful friends who are dear to us",
+		"gather near to us once more",
+
+		"Through the years we all will be together",
+		"If the Fates allow",
+		"Hang a shining star upon the highest bough",
+		"And have yourself a merry little Christmas now"
+		];
+	localStorage.setItem("array", array);
+});
